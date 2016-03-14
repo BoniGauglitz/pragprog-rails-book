2af383482fd4d2fde53342262e7c6cbaf9849c4a
Bruno Gauglitz <bruno.gauglitz@gmail.com>
2016-03-14 00:12:35 -0300

    Learning about ruby methods.

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -2,6 +2,11 @@ class SayController < ApplicationController
   def hello
     @time = Time.now
     @cowboy = concat_cow_boy
+    @local_files = get_local_directory_files
+  end
+
+  def get_local_directory_files
+    return Dir.glob('*')
   end
 
   def concat_cow_boy
